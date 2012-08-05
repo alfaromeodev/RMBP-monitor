012be2678bc964788c661e75a69988f96fbd0f7e
zhang da <ferruccio@localhost.(none)>
2012-08-05 12:12:48 +0800

    init commit

---
+++ Popup/BackgroundView.h
@@ -0,0 +1,10 @@
+#define ARROW_WIDTH 12
+#define ARROW_HEIGHT 8
+
+@interface BackgroundView : NSView {
+    NSInteger _arrowX;
+}
+
+@property (nonatomic, assign) NSInteger arrowX;
+
+@end
---
+++ Popup/Panel.m
@@ -0,0 +1,9 @@
+#import "Panel.h"
+
+@implementation Panel
+
+- (BOOL)canBecomeKeyWindow; {
+    return YES; // Allow Search field to become the first responder
+}
+
+@end
